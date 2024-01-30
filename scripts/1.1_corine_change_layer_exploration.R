257de6024ab032a96428791f6d726a4231f8e373
Beatrice Maria Trascau <beatrimt@vm-srv-franklin.win.ntnu.no>
2024-01-30 14:26:46 +0100

    Add code to download layers from box and read layers in

--- scripts/1.1_corine_change_layer_exploration.R
+++ scripts/1.1_corine_change_layer_exploration.R
@@ -6,3 +6,24 @@
 ##------------------------------------------------##
 
 # This script contains code which explores the CORINE land cover CHANGE layers and creates summary statistics
+
+# 0. PACKAGES ----
+library(here)
+library(terra)
+library(sf)
+library(geodata)
+library(mapview)
+
+# 1. READ IN MODIFIED CORINE CHANGE LAYERS ----
+
+## 1.1. Download layers (if needed) ----
+
+# Add download link from box
+# norway_corine_change_stack <- ("https://ntnu.box.com/shared/static/97g9x4839ij4lnlldji2wh8e0e2lm5bf.tif")
+
+# Download the files
+# download.file(norway_corine_change_stack, "norway_corine_change_stack.tif")
+
+## 1.2. Read in layers -----
+norway_corine_change_stack <- rast(here("data", 
+                                        "norway_corine_change_stack.tif"))
