6cbe83ae0109dcec00f3422f49cb7776a9fd6eea
Beatrice Maria Trascau <beatrimt@vm-srv-franklin.win.ntnu.no>
2024-01-30 14:13:19 +0100

    New script to explore CORINE CHANGE Layers

---
+++ scripts/1.1_corine_change_layer_exploration.R
@@ -0,0 +1,8 @@
+##------------------------------------------------##
+##---------  CODE FOR DATA ANALYSIS FOR ----------##
+##---  "Paper 1: CORINE Land Cover Changes  ------##
+##-------  and GBIF Biodiversity Records  --------##
+##----- 1.1_corine_change_layer_exploration ------##
+##------------------------------------------------##
+
+# This script contains code which explores the CORINE land cover CHANGE layers and creates summary statistics
