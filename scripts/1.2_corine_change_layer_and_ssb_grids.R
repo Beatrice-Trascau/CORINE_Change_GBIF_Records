064cc23e4247a0ac6ada0b332a6205666fb06b1f
Beatrice Maria Trascau <beatrimt@vm-srv-franklin.win.ntnu.no>
2024-02-20 11:18:57 +0100

    Add download links and read in layers

--- scripts/1.2_corine_change_layer_and_ssb_grids.R
+++ scripts/1.2_corine_change_layer_and_ssb_grids.R
@@ -13,3 +13,22 @@ library(data.table)
 library(tidyverse)
 library(dplyr)
 library(ggplot2)
+
+# 1. LOAD DATA ----
+
+# Add download link from box
+# norway_corine_change_stack <- ("https://ntnu.box.com/shared/static/97g9x4839ij4lnlldji2wh8e0e2lm5bf.tif")
+# ruter500m_Norge <- ("https://ntnu.box.com/shared/static/p8896x2epq4bcmfhorsb5qn2m8mxo5ko.zip")
+
+# Download files
+# download.file(norway_corine_change_stack, here("data", 
+                                 # "norway_corine_change_stack.tif"))
+
+# download.file(ruter500m_Norge, here("data", "raw_data",
+                                  # "ruter500m_Norge.zip"))
+
+# Read in the layers
+norway_corine_change_modified_stack <- rast(here("data", 
+                                                 "norway_corine_change_modified_stack.tif"))
+
+ssb_grids <- vect(here("data", "raw_data", "ruter500m_Norge.shp")) 
