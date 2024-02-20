0235311d0e33c1dbe2faef5c9594278a29b8d326
Beatrice Maria Trascau <beatrimt@vm-srv-franklin.win.ntnu.no>
2024-02-20 10:54:24 +0100

    Load libraries

--- scripts/1.2_corine_change_layer_and_ssb_grids.R
+++ scripts/1.2_corine_change_layer_and_ssb_grids.R
@@ -4,3 +4,12 @@
 # This script contains code which combines the CORINE land cover CHANGE layers
 # with the ssb administrative grids for future analyses
 ##----------------------------------------------------------------------------##
+
+# 0. PACKAGES ----
+library(here)
+library(terra)
+library(sf)
+library(data.table)
+library(tidyverse)
+library(dplyr)
+library(ggplot2)
