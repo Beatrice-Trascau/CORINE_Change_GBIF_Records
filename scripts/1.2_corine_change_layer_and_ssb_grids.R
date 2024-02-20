1383cb53a4b500d8cdd1dbddff0fe68e3c608589
Beatrice Maria Trascau <beatrimt@vm-srv-franklin.win.ntnu.no>
2024-02-20 10:39:36 +0100

    New script to combine CORINE Layers and SSB grids for future analyses

---
+++ scripts/1.2_corine_change_layer_and_ssb_grids.R
@@ -0,0 +1,6 @@
+##----------------------------------------------------------------------------##
+# PAPER 1: CORINE LAND COVER CHANGES AND GBIF BIODIVERSITY RECORDS
+# 1.2_corine_change_layer_and_ssb_grids
+# This script contains code which combines the CORINE land cover CHANGE layers
+# with the ssb administrative grids for future analyses
+##----------------------------------------------------------------------------##
