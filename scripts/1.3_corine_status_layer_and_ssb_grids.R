9fededb5ba3072d1bdbbf552b81041b5fb2126ff
Beatrice Maria Trascau <beatrimt@vm-srv-franklin.win.ntnu.no>
2024-04-05 16:00:29 +0200

    Add gbif occurrence records and match projections

--- scripts/1.3_corine_status_layer_and_ssb_grids.R
+++ scripts/1.3_corine_status_layer_and_ssb_grids.R
@@ -37,3 +37,39 @@ norway_corine_status_modified_stack <- rast(here("data",
 ssb_grids <- vect(here("data", "raw_data", "ruter500m_Norge.shp"))
 
 occurrences_norway <- fread(here("data", "cleaned_occurrences.txt"))
+
+# 2. ADD GBIF OCCURRENCE RECORDS ----
+
+## 2.1. Re-project CORINE STATUS and SSB grids to match the occurrences ----
+
+# Check projections
+crs(norway_corine_status_modified_stack, proj = TRUE) #"+proj=laea +lat_0=52 +lon_0=10 +x_0=4321000 +y_0=3210000 +ellps=GRS80 +units=m +no_defs"
+crs(ssb_grids, proj = TRUE) #"+proj=utm +zone=33 +datum=WGS84 +units=m +no_defs"
+
+# Re-project SSB grids to match the projection of occurrences
+norway_ssb_grids <- terra::project(ssb_grids,
+                                   "+proj=longlat +datum=WGS84 +no_defs")
+
+# Re-project CORINE STATUS to match the projection of occurrences
+corine_status_wgs84 <- project(norway_corine_status_modified_stack, 
+                        "+proj=longlat +datum=WGS84 +no_defs", method = "near")
+
+# Check that projections match
+crs(norway_ssb_grids, proj = TRUE) # "+proj=longlat +datum=WGS84 +no_defs"
+crs(corine_status_wgs84[[1]], proj = TRUE) #"+proj=longlat +datum=WGS84 +no_defs"
+
+# Check values for reprojected CORINE layer
+levels(as.factor(as.data.frame(corine_status_wgs84[[1]])$U2006_CLC2000_V2020_20u1))
+
+## 2.2. Convert occurrences to spatial object ----
+
+# Convert occurrences to spatial dataframe ----
+occurrences_sp <- st_as_sf(occurrences_norway, 
+                           coords=c("decimalLongitude","decimalLatitude"),
+                           crs=crs(corine_status_wgs84))
+
+# Convert occurrences to spatial vector ----
+occurrences_vect <- vect(occurrences_sp)
+
+
+
