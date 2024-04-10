d1fc25c4961a1c842bff8e29f3f867462ee49769
Beatrice Maria Trascau <beatrimt@vm-srv-franklin.win.ntnu.no>
2024-04-10 19:18:38 +0200

    Identifu SSB IDs for cover change = Y and N, plot violins instead of barplots

--- scripts/1.3_corine_status_layer_and_ssb_grids.R
+++ scripts/1.3_corine_status_layer_and_ssb_grids.R
@@ -140,8 +140,14 @@ occ_df_2000_2006_richness  <- occ_df_2000_2006  |>
     cover_change = first(cover_change),
     SSBid = first(SSBid))
 
+# Identify SSB IDs that have values for both cover_change = Y and cover_change = N
+ssb_ids_with_both <- occ_df_2000_2006_richness |>
+  group_by(SSBid) |>
+  filter(all(c("Y", "N") %in% cover_change)) |>
+  ungroup()
+
 # Calculate the number of unique corine_cell_IDs for each SSBid
-ssbid_counts <- occ_df_2000_2006_richness |>
+ssbid_counts <- ssb_ids_with_both |>
   group_by(SSBid) |>
   summarise(unique_corine_count = n_distinct(corine_cell_ID)) |>
   ungroup()
@@ -149,26 +155,36 @@ ssbid_counts <- occ_df_2000_2006_richness |>
 # Identify top 10 SSBids with most unique corine_cell_ID values
 top_ssbids <- ssbid_counts |>
   arrange(desc(unique_corine_count)) |>
-  slice_head(n = 10)
+  slice_head(n = 5)
   
 # Filter dataframe to only includes rows corresponding to the top 10 SSBids
 filtered_occ_df_2000_2006_richness <- occ_df_2000_2006_richness |>
   filter(SSBid %in% top_ssbids$SSBid)
 
+# Map land cover values to their names
+land_cover_mapping <- setNames(c("Urban", "Complex Agriculture", "Agriculture & Natural Vegetation", 
+                                 "Forests", "Moors & Grasslands", "Transitional Woodland Shrub", 
+                                 "Sparsely Vegetated Areas"), 
+                               c(1, 80, 103, 250, 380, 590, 711))
+
+# Change values in df based on mapping
+filtered_occ_df_2000_2006_richness$land_cover_2000 <- factor(filtered_occ_df_2000_2006_richness$land_cover_2000,
+                                                             levels = names(land_cover_mapping),
+                                                             labels = land_cover_mapping)
+
 # Subset data based on unique combination of land cover in 2000 and SSB ID 
-filtered_occurrences_2000_2006 <- occurrences_df_2000_2006_richness |>
-  group_by(land_cover_2000, TARGET_FID) |>
+filtered_occ_2000_2006 <- filtered_occ_df_2000_2006_richness |>
+  group_by(land_cover_2000, SSBid) |>
   filter(n() > 1) |>
   ungroup()
 
 # Plot comparison for each combination
-ggplot(filtered_occurrences_2000_2006, 
+ggplot(filtered_occ_2000_2006, 
        aes(x = cover_change, y = species_richness, fill = cover_change)) +
-  geom_bar(stat = "summary", fun = "mean", position = "dodge") +
-  facet_wrap(~ land_cover_2000 + SSBid, scales = "free_x") +
-  labs(title = "Species Richness by Cover Change for Top 10 SSBids", 
-       x = "Cover Change", 
-       y = "Average Species Richness") +
+  geom_violin(trim = FALSE) +
+  facet_wrap(~ land_cover_2000 + SSBid, scales = "free_y") +
+  labs( x = "Cover Change", 
+       y = "Species Richness") +
   theme_minimal()
 
 
