1bcdae1503fd996fdaa4bff3f5af4279234cace0
Beatrice Maria Trascau <beatrimt@vm-srv-franklin.win.ntnu.no>
2024-05-23 11:35:16 +0200

    New script to compare number of occurrences between pixels

---
+++ scripts/2.1_corine_status_layer_occurrences.R
@@ -0,0 +1,7 @@
+##----------------------------------------------------------------------------##
+# PAPER 1: CORINE LAND COVER CHANGES AND GBIF BIODIVERSITY RECORDS
+# 2.1_corine_status_layer_occurrences
+# This script compares the number of occurrence records in CORINE land cover
+# STATUS pixels that change land cover through time to those that do not change
+##----------------------------------------------------------------------------##
+
