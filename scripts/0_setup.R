ca99f02b6977bb56951f8cef9a53e8a2b8264d21
Beatrice Maria Trascau <beatrimt@vm-srv-franklin.win.ntnu.no>
2024-05-31 20:10:12 +0200

    Added more packages

--- scripts/0_setup.R
+++ scripts/0_setup.R
@@ -16,7 +16,7 @@ package_vec <- c("here", "terra", "sf", "geodata", "mapview",
                  "tidyverse", "dplyr", "ggplot2", "ggalluvial",
                  "networkD3", "gt", "cowplot", "data.table",
                  "tidyterra", "patchwork", "styler", "scales",
-                 "plotly", "lme4", "DHARMa") # specify packages
+                 "plotly", "lme4", "DHARMa", "glmmTMB") # specify packages
 
 ## executing install & load for each package
 sapply(package_vec, install.load.package)
