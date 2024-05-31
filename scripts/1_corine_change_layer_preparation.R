f932ecc4cd3def05ae81259c8fe91b7447812da1
Beatrice Maria Trascau <beatrimt@vm-srv-franklin.win.ntnu.no>
2024-05-31 20:11:52 +0200

    Changing script to use functions instead of repeating the same actions twice. Added functions to download and read in the rasters.

--- scripts/1_corine_change_layer_preparation.R
+++ scripts/1_corine_change_layer_preparation.R
@@ -1,10 +1,88 @@
 ##----------------------------------------------------------------------------##
 # PAPER 1: CORINE LAND COVER CHANGES AND GBIF BIODIVERSITY RECORDS
-# 1_corine_change_layer_preparation
+# 1.1_corine_layers_preparation
 # This script contains code which loads and prepares the CORINE land cover 
-# CHANGE layers for analysis
-# and the CORINE land cover STATUS layers for analysis
+# CHANGE and STATUS layers for further analysis
 ##----------------------------------------------------------------------------##
+
+# 1. DEFINE FUNCTIONS ----------------------------------------------------------
+
+## 1.1. Function to download files if they do not yet exist --------------------
+download_files <- function(urls, filenames, dir = "data/raw_data") {
+  if (!dir.exists(dir)) dir.create(dir, recursive = TRUE)
+  for (i in seq_along(urls)) {
+    file_path <- file.path(dir, filenames[i])
+    if (!file.exists(file_path)) {
+      download.file(urls[i], file_path)
+    }
+  }
+}
+
+## 1.2. Function to read rasters -----------------------------------------------
+read_rasters <- function(filenames, dir = "data/raw_data") {
+  rasters <- lapply(filenames, function(x) rast(here(dir, x)))
+  return(do.call(c, rasters))
+}
+
+## 1.3. Function to crop and mask rasters to Norway ----------------------------
+crop_mask_to_norway <- function(raster_stack, norway_shape) {
+  return(crop(raster_stack, norway_shape, mask = TRUE))
+}
+
+## 1.4. Function to modify classes in the rasters ------------------------------
+modify_class_values <- function(raster_stack, class_modifications) {
+  modified_stack <- raster_stack
+  for (mod in class_modifications) {
+    modified_stack <- app(modified_stack, fun = function(x) {
+      x[x %in% mod$from] <- mod$to
+      return(x)
+    })
+  }
+  return(modified_stack)
+}
+
+## 1.5. Define URLs, names and new values for rasters --------------------------
+
+# URLs and Filenames for the CORINE Change Layers
+change_urls <- c(
+  "https://ntnu.box.com/shared/static/vduuevecunldbrc7jb60jarjts99c4db.tif",
+  "https://ntnu.box.com/shared/static/nmn2kguk9ipx0u4a2a8yfvcozdf6g9ij.tif",
+  "https://ntnu.box.com/shared/static/pah7ig013inqeepg3gwvfan9w00anitp.tif",
+  "https://ntnu.box.com/shared/static/g9grkxsvv20sz48rkbig8f9tb8gennfy.tif",
+  "https://ntnu.box.com/shared/static/v51lua6b9fph0k7bmsbbc1g20tkdjqh9.tif",
+  "https://ntnu.box.com/shared/static/x7ck0jnagfoxvjxvxf99l9lhknky5xlt.tif")
+
+change_filenames <- c(
+  "U2006_CHA0006_00_V2020_20u1.tif",
+  "U2006_CHA0006_06_V2020_20u1.tif",
+  "U2012_CHA0612_06_V2020_20u1.tif",
+  "U2012_CHA0612_12_V2020_20u1.tif",
+  "U2018_CHA1218_12_V2020_20u1.tif",
+  "U2018_CHA1218_18_V2020_20u1.tif")
+
+# URLs and Filenames for the CORINE Status Layers
+status_urls <- c(
+  "https://ntnu.box.com/shared/static/ffmbbb89aikwg64tg9ei30c8fnf7chl2.tif",
+  "https://ntnu.box.com/shared/static/2x6g9jaov5rex3u0xt3hq9mmy91d63ew.tif",
+  "https://ntnu.box.com/shared/static/ut1pcbnj7xgfwv3ptahu5c3krdy24l7d.tif",
+  "https://ntnu.box.com/shared/static/iub514rfjnkopg3nu4nc18j4axq5jfon.tif")
+
+status_filenames <- c(
+  "U2006_CLC2000_V2020_20u1.tif",
+  "U2012_CLC2006_V2020_20u1.tif",
+  "U2018_CLC2012_V2020_20u1.tif",
+  "U2018_CLC2018_V2020_20u1.tif")
+
+# Changes for classes in the rasters
+class_modifications <- list(
+  list(from = 1:11, to = 1),
+  list(from = c(12, 18, 20), to = 80),
+  list(from = 21, to = 103),
+  list(from = c(23, 24, 25), to = 250),
+  list(from = c(26, 27), to = 380),
+  list(from = 29, to = 590),
+  list(from = 32, to = 711),
+  list(from = c(30, 31, 33, 34, 35, 36, 39, 40, 41, 43, 44, 127, 128), to = NA))
 
 # 0. PACKAGES ----
 library(here)
@@ -13,40 +91,16 @@ library(sf)
 library(geodata)
 library(mapview)
 
-# 1. READ IN CORINE CHANGE LAYERS ----
+# 1. READ IN CORINE LAYERS -----------------------------------------------------
 
-## 1.1. Download change layers (if needed) ----
+## 1.1. Download layers (if needed) ----
+download_files(change_urls, change_filenames)
+download_files(status_urls, status_filenames)
 
-# Add download link from box
-# U2006_CHA0006_00_V2020_20u1 <- ("https://ntnu.box.com/shared/static/vduuevecunldbrc7jb60jarjts99c4db.tif")
-# U2006_CHA0006_06_V2020_20u1 <- ("https://ntnu.box.com/shared/static/nmn2kguk9ipx0u4a2a8yfvcozdf6g9ij.tif")
-# U2012_CHA0612_06_V2020_20u1 <- ("https://ntnu.box.com/shared/static/pah7ig013inqeepg3gwvfan9w00anitp.tif")
-# U2012_CHA0612_12_V2020_20u1 <- ("https://ntnu.box.com/shared/static/g9grkxsvv20sz48rkbig8f9tb8gennfy.tif")
-# U2018_CHA1218_12_V2020_20u1 <- ("https://ntnu.box.com/shared/static/v51lua6b9fph0k7bmsbbc1g20tkdjqh9.tif")
-# U2018_CHA1218_18_V2020_20u1 <- ("https://ntnu.box.com/shared/static/x7ck0jnagfoxvjxvxf99l9lhknky5xlt.tif")
 
-# Download the files
-# download.file(U2006_CHA0006_00_V2020_20u1, "U2006_CHA0006_00_V2020_20u1.tif")
-# download.file(U2006_CHA0006_06_V2020_20u1, "U2006_CHA0006_06_V2020_20u1.tif")
-# download.file(U2012_CHA0612_06_V2020_20u1, "U2012_CHA0612_06_V2020_20u1.tif")
-# download.file(U2012_CHA0612_12_V2020_20u1, "U2012_CHA0612_12_V2020_20u1.tif")
-# download.file(U2018_CHA1218_12_V2020_20u1, "U2018_CHA1218_12_V2020_20u1.tif")
-# download.file(U2018_CHA1218_18_V2020_20u1, "U2018_CHA1218_18_V2020_20u1.tif")
-
-## 1.2. Read in change layers -----
-
-# Read in the CORINE CHANGE layers
-corine_change0006_00 <- rast(here("data", "raw_data", "U2006_CHA0006_00_V2020_20u1.tif"))
-corine_change0006_06 <- rast(here("data", "raw_data", "U2006_CHA0006_06_V2020_20u1.tif"))
-corine_change0612_06 <- rast(here("data", "raw_data", "U2012_CHA0612_06_V2020_20u1.tif"))
-corine_change0612_12 <- rast(here("data", "raw_data", "U2012_CHA0612_12_V2020_20u1.tif"))
-corine_change1218_12 <- rast(here("data", "raw_data", "U2018_CHA1218_12_V2020_20u1.tif"))
-corine_change1218_18 <- rast(here("data", "raw_data", "U2018_CHA1218_18_V2020_20u1.tif"))
-
-# Stack layers into 1 object
-corine_change_stack <- c(corine_change0006_00, corine_change0006_06,
-                         corine_change0612_06, corine_change0612_12,
-                         corine_change1218_12, corine_change1218_18)
+## 1.2. Read in  layers --------------------------------------------------------
+corine_change_stack <- read_rasters(change_filenames)
+corine_status_stack <- read_rasters(status_filenames)
 
 # 2. CUT AND MASK CHANGE LAYERS TO NORWAY ----
 
