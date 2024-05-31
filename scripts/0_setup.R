8082a2ef8ac8f84d8fc58ec50801b890c314ca92
Beatrice Maria Trascau <beatrimt@vm-srv-franklin.win.ntnu.no>
2024-05-31 17:58:09 +0200

    Added more packages and comments

--- scripts/0_setup.R
+++ scripts/0_setup.R
@@ -1,3 +1,10 @@
+##----------------------------------------------------------------------------##
+# PAPER 1: CORINE LAND COVER CHANGES AND GBIF BIODIVERSITY RECORDS
+# 0_setup
+# This script contains code which loads/installs necessary packages
+##----------------------------------------------------------------------------##
+
+# Function to load/install packages needed for analysis
 install.load.package <- function(x) {
   if (!require(x, character.only = TRUE)) {
     install.packages(x, repos = "http://cran.us.r-project.org")
@@ -5,21 +12,17 @@ install.load.package <- function(x) {
   require(x, character.only = TRUE)
 }
 
-package_vec <- c("here","dplyr","knitr",
-                 "ggplot2","rgbif",
-                 "here", "CoordinateCleaner",
-                 "terra", "sf","mapview",
-                 "PointedSDMs","stringr",
-                 "stars", "geodata") # specify packages
+package_vec <- c("here", "terra", "sf", "geodata", "mapview",
+                 "tidyverse", "dplyr", "ggplot2", "ggalluvial",
+                 "networkD3", "gt", "cowplot", "data.table",
+                 "tidyterra", "patchwork", "styler", "scales",
+                 "plotly", "lme4", "DHARMa") # specify packages
 
 ## executing install & load for each package
 sapply(package_vec, install.load.package)
 
-# INLA is not on CRAN, so has to be installed separatly
-#install.packages("INLA",repos=c(getOption("repos"),INLA="https://inla.r-inla-download.org/R/stable"), dep=TRUE) 
-library("INLA")
 
-
+# Function to only dowload files that are not already in the folders
 conditional_download <- function(url, target) {
   if (!file.exists(target)) {
     download.file(url=url, destfile=target)
