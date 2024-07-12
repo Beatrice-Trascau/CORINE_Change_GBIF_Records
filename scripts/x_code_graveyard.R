967e6ff1e345c5ce5c21ff5dfd95b1edeb0c3668
Beatrice-Trascau <beatrice.m.trascau@ntnu.no>
2024-07-12 17:41:32 +0200

    Added code from script 3.6_cover_change_types_occ_mode_offset_setup

--- scripts/x_code_graveyard.R
+++ scripts/x_code_graveyard.R
@@ -2593,3 +2593,163 @@ model2.5_nb <- glmmTMB(ocurrences_after ~ cover_change * time_period * ocurrence
 save(model2.5_nb, file = here::here("data", "models", "model2.5_nb.RData"))
 
 # END OF SCRIPT ----------------------------------------------------------------
+
+##----------------------------------------------------------------------------##
+# PAPER 1: CORINE LAND COVER CHANGES AND GBIF BIODIVERSITY RECORDS
+# 3.5_cover_change_types_occ_model_offset_setup
+# This script contains code which runs the simple models looking at the effect 
+# of land cover change types and offset on the number of occurrences in a pixel
+##----------------------------------------------------------------------------##
+
+# 0. LOAD PACKAGES -------------------------------------------------------------
+library(here)
+library(dplyr)
+library(lattice)
+library(lme4)
+library(DHARMa)
+library(glmmTMB)
+library(mgcv)
+library(gamm4)
+library(ggplot2)
+
+# 1. PREPARE DATA FOR MODELS ---------------------------------------------------
+
+# Load data
+load(here("data", "derived_data", 
+          "occurrences_SSB_municipalities_land_cover.rda"))
+
+# Rename df (to make it easier to work with)
+occ_SSB_land_cover <- occurrence_municipalities_df
+
+## 1.1. First period of change: 2000-2006 --------------------------------------
+
+# Change column names for easier df manipulation
+occ_SSB_land_cover <- occ_SSB_land_cover |>
+  rename(land_cover2000 = U2006_CLC2000_V2020_20u1,
+         land_cover2006 = U2012_CLC2006_V2020_20u1,
+         land_cover2012 = U2018_CLC2012_V2020_20u1,
+         land_cover2018 = U2018_CLC2018_V2020_20u1)
+
+# Prepare data for the period 2006-2009
+occ_df_before_2000.2006 <- occ_SSB_land_cover |>
+  select(V1, gbifID, year, species, land_cover2000, land_cover2006, 
+         SSBID, cell_ID, NAME_2) |>
+  filter(!is.na(land_cover2000) & !is.na(land_cover2006)) |>
+  filter(year >= 1997 & year <= 2000) |>
+  mutate(cover_change = land_cover2000 - land_cover2006)
+
+# Calculate number of records for the period 2006-2009
+occ_df_before_2000.2006_records <- occ_df_before_2000.2006 |>
+  group_by(cell_ID) |>
+  summarise(
+    ocurrences_before = n(),
+    land_cover2000 = first(land_cover2000),
+    land_cover2006 = first(land_cover2006),
+    cover_change = first(cover_change),
+    SSBID = first(SSBID),
+    NAME_2 = first(NAME_2)) |>
+  mutate(time_period = "2000-2006") |>
+  select(-c(land_cover2000, land_cover2006))
+
+## 1.2. Second period of change: 2006 - 2012 -----------------------------------
+
+# Prepare data for the period 2003-2006
+occ_df_before_2006.2012 <- occ_SSB_land_cover |>
+  select(V1, gbifID, year, species, land_cover2006, land_cover2012, 
+         SSBID, cell_ID, NAME_2) |>
+  filter(!is.na(land_cover2006) & !is.na(land_cover2012)) |>
+  filter(year >= 2003 & year <= 2006) |>
+  mutate(cover_change = land_cover2006 - land_cover2012)
+
+# Calculate number of records for the period 2003-2006
+occ_df_before_2006.2012_records <- occ_df_before_2006.2012 |>
+  group_by(cell_ID) |>
+  summarise(
+    ocurrences_before = n(),
+    land_cover2006 = first(land_cover2006),
+    land_cover2012 = first(land_cover2012),
+    cover_change = first(cover_change),
+    SSBID = first(SSBID),
+    NAME_2 = first(NAME_2)) |>
+  mutate(time_period = "2006-2012") |>
+  select(-c(land_cover2006, land_cover2012))
+
+## 1.3. Third period of change: 2012 - 2018 ------------------------------------
+
+# Prepare data for the period 2009-2012
+occ_df_before_2012.2018 <- occ_SSB_land_cover |>
+  select(V1, gbifID, year, species, land_cover2012, land_cover2018, 
+         SSBID, cell_ID, NAME_2) |>
+  filter(!is.na(land_cover2012) & !is.na(land_cover2018)) |>
+  filter(year >= 2009 & year <= 2012) |>
+  mutate(cover_change = land_cover2012 - land_cover2018)
+
+# Calculate number of records for the period 1997-2000
+occ_df_before_2012.2018_records <- occ_df_before_2012.2018 |>
+  group_by(cell_ID) |>
+  summarise(
+    ocurrences_before = n(),
+    land_cover2012 = first(land_cover2012),
+    land_cover2018 = first(land_cover2018),
+    cover_change = first(cover_change),
+    SSBID = first(SSBID),
+    NAME_2 = first(NAME_2)) |>
+  mutate(time_period = "2012-2018") |>
+  select(-c(land_cover2012, land_cover2018))
+
+## 1.4. Combine the above 3 into a single df -----------------------------------
+
+occ_df_before_records <- bind_rows(occ_df_before_2000.2006_records,
+                                   occ_df_before_2006.2012_records,
+                                   occ_df_before_2012.2018_records) |>
+  select(-cell_ID) |>
+  rename(municipality = NAME_2)
+
+## 1.5. Combine occ_df_before_records wtih occ_df_after_records ----------------
+
+# Read in dataframe with occurrences after a change
+load(here("data", "derived_data", 
+          "occ_cover_change_types_after_records_for_model.rda"))
+
+# Full join of the dfs on SSBid and time_period
+occ_df_before_after <- full_join(occ_cover_change_types_after_records_for_model,
+                                 occ_df_before_records,
+                                 by = c("SSBID", "time_period", 
+                                        "cover_change", "municipality"))
+
+# Replace NA with 0 for occurrences_before and occurrences_after
+occ_cover_change_types_before_after_for_model <- occ_df_before_after |>
+  mutate(ocurrences_after = ifelse(is.na(ocurrences_after), 0, ocurrences_after),
+         ocurrences_before = ifelse(is.na(ocurrences_before), 0, ocurrences_before))
+
+## 1.6. Write dataframe to file ------------------------------------------------
+save(occ_cover_change_types_before_after_for_model, 
+     file = here::here("data", "derived_data",
+                       "occ_cover_change_types_before_after_for_model.rda"))
+
+# 2. MODEL 2: OCC ~ COVER CHANGE + OFFSET --------------------------------------
+
+## 2.1. N binomial glmmTMB, nbinom 2, SSBID on data subset ---------------------
+
+# Create 10% subset of the data
+subset_data_cover_change_types_before_after <- occ_cover_change_types_before_after_for_model |>
+  sample_frac(0.1)
+
+# Save subset
+save(subset_data_cover_change_types_before_after, 
+     file = here::here("data", "derived_data", 
+                       "subset_data_cover_change_types_before_after.rda"))
+
+# Load data subset
+# load(here("data", "derived_data", 
+#           "subset_data_cover_change_types_before_after.rda"))
+
+# Run model
+model6.1 <- glmmTMB(ocurrences_after ~ cover_change * time_period * ocurrences_before + offset(ocurrences_before) + (1 | SSBID),
+                    family = nbinom2,
+                    data = subset_data_cover_change_types_before_after)
+
+# Save model output to file to save time next time
+save(model6.1, file = here::here("data", "models", "model6.1.RData"))
+
+# END OF SCRIPT ----------------------------------------------------------------
