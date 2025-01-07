b762354cf52fc8c766dca82b33716200098097f6
beatrimt <beatrice.m.trascau@ntnu.no>
2025-01-07 14:51:41 +0100

    Added code to plot add heatmap with geom_points

--- scripts/5.1_cover_change_combine_model_outputs_in_figure4.R
+++ scripts/5.1_cover_change_combine_model_outputs_in_figure4.R
@@ -164,14 +164,8 @@ a <- ggplot(cover_effect_no_year_interaction,
 
 # Reorder the levels of initial land cover column
 time_effect_no_year_interaction <- time_effect_no_year_interaction |>
-  mutate(intial_cover = factor(intial_cover, 
-                               levels = c("Urban Fabric",
-                                          "Transitional Woodland Shrub",
-                                          "Sparse Vegetation",
-                                          "Moors, Heathland & Grassland",
-                                          "Forests",
-                                          "Complex Agriculture",
-                                          "Agriculture & Vegetation")))
+  mutate(intial_cover = factor(label_mapping[as.character(intial_cover)]))
+
 # Define breaks for the time legend
 legend_breaks_time <- c(min(time_effect_no_year_interaction$Significant, na.rm = TRUE), 
                    0, -0.4, -0.8,
@@ -182,7 +176,7 @@ legend_breaks_time <- c(min(time_effect_no_year_interaction$Significant, na.rm =
 time <- ggplot(time_effect_no_year_interaction, 
                aes(x = cover_change, y = intial_cover)) +
   geom_tile(aes(fill = Significant), color = "black", size = 0.5) +
-  scale_fill_gradient2(low = "blue", mid = "white", high = "red", 
+  scale_fill_gradient2(low = "#0072B2", mid = "white", high = "red", 
                        midpoint = 0, na.value = "grey90", name = "Estimate", 
                        breaks = legend_breaks_time, 
                        labels = scales::label_number()) +
@@ -202,60 +196,110 @@ cover_time_combined_plot <- plot_grid(a, time,
                            ncol = 1, labels = c("a)", "b)"))
 
 # Save to file as .png
-ggsave(here("figures", "model_outputs_Figure4.png"),
+ggsave(here("figures", "model_outputs_Figure4.5.png"),
        width=20, height=13)
 
-# 5. PLOT EFFECTS WITH INTERACTION WITH 2006-2012 ------------------------------
+# 5. PLOT EFFECTS AS POINTS WITH ERROR BARS ------------------------------------
 
-# Subset df to only contain rows with 2006-2012 interaction
-effect_2006_2012_interaction <- combined_results5 |>
-  filter(time_period == "time.period2006-2012") |>
-  mutate(Significant = ifelse(`Pr(>|z|)` < p_value_threshold, Estimate, NA)) |>
-  filter(!is.na(cover_change))
+# Create a label mapping
+label_mapping <- c("Agriculture & Vegetation" = "ASNV",
+  "Complex Agriculture" = "CA",
+  "Forests" = "Forests",
+  "Moors, Heathland & Grassland" = "MHG",
+  "Sparse Vegetation" = "SV",
+  "Transitional Woodland Shrub" = "TWS",
+  "Urban Fabric" = "UF")
 
-# Plot heatmap
-b <- ggplot(effect_2006_2012_interaction, 
-            aes(x = cover_change, y = intial_cover, fill = Significant)) +
-  geom_tile(color = "white") +
-  scale_fill_gradientn(colors = c("blue", "white", "red"), na.value = "grey90", name = "Estimate") +
-  scale_x_discrete(position = "top") +  
+# Create a new order for the factors
+new_order <- c("UF", "TWS", "SV", "MHG", "Forests", "CA", "ASNV")
+
+# Change df to fit the plot
+cover_points <- cover_effect_no_year_interaction |>
+  mutate(intial_cover = factor(label_mapping[as.character(intial_cover)], levels = rev(new_order)),
+    cover_change = factor(label_mapping[as.character(cover_change)]),
+    plot_type = case_when(
+      is_same ~ "same",
+      is.na(Significant) ~ "non_significant",
+      !is.na(Significant) ~ "significant")) |>
+  rename(std_error = 'Std. Error')
+
+# Calculate y-axis with only the significant results
+significant_results <- cover_points |>
+  filter(!is.na(Significant))
+
+# Calculate ymin and ymax
+y_min <- min(significant_data$Estimate - significant_data$std_error, 
+             na.rm = TRUE)
+y_max <- max(significant_data$Estimate + significant_data$std_error, 
+             na.rm = TRUE)
+
+# Create a df with break points for y axis
+y_breaks_df <- cover_points |>
+  filter(plot_type == "significant") |>
+  distinct(intial_cover, cover_change) |>
+  crossing(y_breaks = pretty(c(y_min, y_max), n = 4))
+
+# Plot "heatmap" with points
+cover_points_plot <- ggplot() +
+  # Create facets for each combination of initial land cover and cover change
+  facet_grid(intial_cover ~ cover_change, switch = "y") +
+  # Add black background where initial_cover = cover_change
+  geom_rect(data = cover_effect_no_year_interaction |> 
+              filter(plot_type == "same"), 
+            aes(xmin = -Inf, xmax = Inf, ymin = -Inf, ymax = Inf), 
+            fill = "black") +
+  # Add y-axis text for significant results
+  geom_text(data = y_breaks_df,
+            aes(x = 0.4, y = y_breaks, label = format(y_breaks, digits = 2)),
+            hjust = 1,
+            size = 3) +
+  # Add elements only for significant plots
+  geom_hline(data = cover_effect_no_year_interaction |>
+               filter(plot_type == "significant"), 
+             aes(yintercept = 0), 
+             linetype = "dotted") +
+  # Add error bars and points
+  geom_errorbar(data = cover_effect_no_year_interaction |>
+                  filter(plot_type == "significant"),
+                aes(x = 0.5, ymin = Estimate - std_error, 
+                    ymax = Estimate + std_error, color = Significant),
+                width = 0.2) +
+  geom_point(data = cover_effect_no_year_interaction |>
+               filter(plot_type == "significant"),
+             aes(x = 0.5, y = Estimate, color = Significant),
+             size = 2) +
+  # Custom scale for point colors
+  scale_color_gradient2(low = "#0072B2", mid = "white", high = "red", 
+                        midpoint = 0, na.value = "grey90", 
+                        name = "Estimate", 
+                        breaks = legend_breaks_cover,
+                        labels = scales::label_number()) +
+  # Set consistent y-axis limits
+  coord_cartesian(ylim = c(y_min, y_max)) +
+  # Theme customization
   theme_classic() +
-  theme(
-    axis.text.x = element_text(angle = 15, hjust = 0, size = 14),
-    axis.text.y = element_text(size = 14),
-    axis.title.x = element_blank(),
+  theme(axis.text.x = element_blank(),
+    axis.ticks.x = element_blank(),
+    axis.title.x = element_text(size = 12, face = "bold"),
+    panel.grid = element_blank(),
+    strip.text.x = element_text(hjust = 0, size = 12),
+    strip.text.y.left = element_text(size = 14, hjust = 1),
+    strip.background = element_blank(),
+    panel.border = element_rect(color = "black", fill = NA),
+    axis.text.y = element_blank(),  
+    axis.ticks.y = element_blank(), 
     axis.title.y = element_text(size = 14, face = "bold"),
-    axis.line = element_blank(),
-    axis.ticks = element_blank()) +
-  labs(y = "Initial Cover")
+    axis.line = element_blank()) +
+  labs(y = "Initial Cover",
+       x = "Cover Change") +
+  scale_x_continuous(position = "top")
 
-# 6. PLOT EFFECTS WITH INTERACTION WITH 2012-2018 ------------------------------
-
-# Subset df to only contain rows with 2006-2012 interaction
-effect_2012_2018_interaction <- combined_results5 |>
-  filter(time_period == "time.period2012-2018") |>
-  mutate(Significant = ifelse(`Pr(>|z|)` < p_value_threshold, Estimate, NA)) |>
-  filter(!is.na(cover_change))
-
-# Plot heatmap
-c <- ggplot(effect_2012_2018_interaction, 
-            aes(x = cover_change, y = intial_cover, fill = Significant)) +
-  geom_tile(color = "white") +
-  scale_fill_gradientn(colors = c("blue", "white", "red"), na.value = "grey90", name = "Estimate") +
-  scale_x_discrete(position = "top") +  
-  theme_classic() +
-  theme(
-    axis.text.x = element_text(angle = 15, hjust = 0, size = 14),
-    axis.text.y = element_text(size = 14),
-    axis.title.x = element_blank(),
-    axis.title.y = element_blank(),
-    axis.line = element_blank(),
-    axis.ticks = element_blank())
-
-# 7. COMBINE AND REFINE PLOTS --------------------------------------------------
-combined_plot <- plot_grid(a, b, c, 
-                           ncol = 1, labels = c("a)", "b)", "c)"))
+# Combine the two plots
+cover_time_point_plot <- plot_grid(cover_points_plot, time,
+                                      ncol = 1, labels = c("a)", "b)"))
 
 # Save to file as .png
-ggsave(here("figures", "model_outputs_Figure4.png"),
+ggsave(here("figures", "model_outputs_Figure4.5.png"),
        width=20, height=13)
+
+# END OF SCRIPT ----------------------------------------------------------------
